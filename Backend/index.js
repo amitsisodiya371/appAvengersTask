f2ab556c5c87b04282fc1f2d4f1f16d61203ddef
amitsisodiya371 <asisodiya9045@gmail.com>
2023-06-12 18:43:08 +0530

    minor changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,46 +1,23 @@
-// const express = require('express');
-const cors = require('cors');
-// const connectDB = require('./config/db');
-// const blogRoutes = require('./routes/blogRoutes');
-
-// const app = express();
-// const port = 3030;
-
-// app.use(cors());
-// // Middleware
-// app.use(express.json());
-
-// // Routes
-// app.use('/api/blogs', blogRoutes);
-
-// // Connect to the database
-// connectDB();
-
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server listing on port ${port}`);
-// });
-
-
 const express = require('express');
-const bodyParser = require('body-parser');
+const cors = require('cors');
 const connectDB = require('./config/db');
-const projectRoutes = require('./routes/routes');
+const blogRoutes = require('./routes/routes');
 
 const app = express();
 const port = 3030;
 
-app.use(bodyParser.json());
-
-
-
 app.use(cors());
+// Middleware
+app.use(express.json());
+
 // Routes
-app.use('/api/project', projectRoutes);
+app.use('/api/blogs', blogRoutes);
 
 // Connect to the database
 connectDB();
+
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server listing on port ${port}`);
 });
+
